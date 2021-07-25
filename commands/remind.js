f8d5260a94d985304af5c8f6036ad84b00b52129
Owleren <>
2021-07-25 10:05:43 +0000

    v0.1.3 patch

--- commands/remind.js
+++ commands/remind.js
@@ -4,11 +4,15 @@ module.exports = {
   name: "remind",
   execute(message, args) {
     const remindMessage = args[0];
-    const time = args[1];
+    const time = args[args.length - 1];
     const user = message.author;
-    message.channel.send("Хорошо, отправлю!");
-    setTimeout(() => {
-      user.send(remindMessage);
-    }, ms(time));
+    try {
+      message.channel.send("Хорошо, отправлю!");
+      setTimeout(() => {
+        user.send(remindMessage);
+      }, ms(time));
+    } catch(e) {
+      message.channel.send("Что-то пошло не так..");
+    }
   }
 };
